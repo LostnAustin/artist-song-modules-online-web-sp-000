8f18b8ed11b04efed6582e4475d9511f599c3749
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-13 17:25:39 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,19 @@
 
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def reset_all
-    all.clear
+    def count
+      self.all.count
+    end
   end
 
-  def count
-    all.count
-  end
+    module InstanceMethods
+      def initialize
+
+        end
+      end
 
 end
